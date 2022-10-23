e7dc013a0d26dcf6a938bca14b43375d67d5c1ee
azeem <pinjariashpak3@gmail.com>
2022-10-23 14:57:49 +0530

    Cart Functions except delete

--- app/src/main/java/com/example/pizzaorderinh/trollyDetails.java
+++ app/src/main/java/com/example/pizzaorderinh/trollyDetails.java
@@ -1,26 +1,32 @@
 package com.example.pizzaorderinh;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.protobuf.Value;
 
 import java.util.HashMap;
 
@@ -31,16 +37,16 @@ public class trollyDetails extends Fragment {
     FirebaseAuth cAuth = FirebaseAuth.getInstance();
     String cUser = cAuth.getCurrentUser().getUid();
     DatabaseReference dFire = fiery.getReference().child("Cart").child(cUser);
-
+    ProgressDialog changes,deleting;
 
 
 
     String quantity,cpImage,cpName,orderTotal;
-    ImageView trimg1,minus,plus;
+    ImageView trimg1,minus,plus,delete;
     TextView trtex1,trtex2,trtex3;
     Button saveNew;
-    String oldStore,newStore;
-    int operate,assign;
+    String stoldStore,stnewStore,stnayaQuantity,stnewOrderTotal;
+    int operate,assign,newQuant,oldQuant,perPieceCost,intOrderTotal,intNewOrderTotal;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -69,6 +75,7 @@ public class trollyDetails extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,40 +95,150 @@ public class trollyDetails extends Fragment {
         trtex3 = viewCartDetails.findViewById(R.id.textQuan);
         minus = viewCartDetails.findViewById(R.id.decre);
         plus = viewCartDetails.findViewById(R.id.incre);
+        delete = viewCartDetails.findViewById(R.id.delbin);
         saveNew = viewCartDetails.findViewById(R.id.saveChange);
+        changes = new ProgressDialog(getContext());
 
         trtex1.setText(cpName);
         trtex2.setText(orderTotal);
         trtex3.setText(quantity);
         Glide.with(getContext()).load(cpImage).into(trimg1);
 
+        oldQuant = Integer.parseInt(quantity);
+        intOrderTotal = Integer.parseInt(orderTotal);
+
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                oldStore = trtex3.getText().toString();
-                operate = Integer.parseInt(oldStore);
+                stoldStore = trtex3.getText().toString();
+                operate = Integer.parseInt(stoldStore);
                 assign = operate-1;
-                newStore = String.valueOf(assign);
-                trtex3.setText(newStore);
+                stnewStore = String.valueOf(assign);
+                trtex3.setText(stnewStore);
             }
         });
 
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                oldStore = trtex3.getText().toString();
-                operate = Integer.parseInt(oldStore);
+                stoldStore = trtex3.getText().toString();
+                operate = Integer.parseInt(stoldStore);
                 assign = operate+1;
-                newStore = String.valueOf(assign);
-                trtex3.setText(newStore);
+                stnewStore = String.valueOf(assign);
+                trtex3.setText(stnewStore);
 
             }
         });
+
+        saveNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stnayaQuantity = trtex3.getText().toString();
+                newQuant = Integer.parseInt(stnayaQuantity);
+                perPieceCost = intOrderTotal/oldQuant;
+                intNewOrderTotal = perPieceCost*newQuant;
+                stnewOrderTotal = String.valueOf(intNewOrderTotal);
+
+                if(newQuant==0||intNewOrderTotal==0){
+
+                }
+
+                HashMap<String,String> changedCart = new HashMap<>();
+                changedCart.put("cpImage",cpImage);
+                changedCart.put("cpName",cpName);
+                changedCart.put("orderTotal",stnewOrderTotal);
+                changedCart.put("quantity",stnayaQuantity);
+
+                ValueEventListener vel = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot ds : snapshot.getChildren()){
+                            String userKey = ds.getKey();
+                            DatabaseReference newdRef = fiery.getReference().child("Cart").child(cUser).child(userKey);
+                            ValueEventListener vel2 = new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if(snapshot==null){
+                                        Toast.makeText(getContext(),"NULL", Toast.LENGTH_LONG).show();
+                                    }
+                                    else if(snapshot.child("cpName").getValue().equals(cpName)){
+                                        newdRef.setValue(changedCart);
+                                        changes.setTitle("Updating To Cart");
+                                        changes.setMessage("Please Wait while changes are saved");
+                                        changes.setCanceledOnTouchOutside(false);
+                                        changes.show();
+                                        sendUserToCart();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            };
+                            newdRef.addListenerForSingleValueEvent(vel2);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                };
+               dFire.addListenerForSingleValueEvent(vel);
+               trtex2.setText(stnewOrderTotal);
+            }
+        });
+
+        ValueEventListener delVel = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    String pDelKey = ds.getKey();
+                    DatabaseReference pDelRef = fiery.getReference().child("Cart").child(cUser).child(pDelKey);
+                    ValueEventListener delVel2 = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot==null){
+                                Toast.makeText(getContext(),"NULL", Toast.LENGTH_LONG).show();
+                            }
+                            else if(snapshot.child("quantity").getValue().equals("0")||snapshot.child("orderTotal").getValue().equals("0")){
+                                pDelRef.setValue(null);
+                                Toast.makeText(getContext(), "REMOVED ITEM FROM CART", Toast.LENGTH_LONG).show();
+                                sendUserToCart();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    };
+                    pDelRef.addListenerForSingleValueEvent(delVel2);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        dFire.addListenerForSingleValueEvent(delVel);
+
+
+
+
+
         return viewCartDetails;
     }
     public void onBackPressedCart()
     {
         AppCompatActivity cartBack = (AppCompatActivity) getContext();
         cartBack.getSupportFragmentManager().beginTransaction().replace(R.id.trolly, new trollyList()).addToBackStack(null).commit();
+    }
+    public void sendUserToCart(){
+        changes.dismiss();
+        AppCompatActivity cartProgress = (AppCompatActivity) getContext();
+        cartProgress.getSupportFragmentManager().beginTransaction().replace(R.id.trolly, new trollyList()).addToBackStack(null).commit();
     }
 }
